14a5f488263b58c79eb8eef7ad25b7e8fdb27915
Marlon ryzen <enginer26mj@gmail.com>
2020-08-07 13:34:48 -0500

    Json request handler is working

--- hash-api.py
+++ hash-api.py
@@ -1,16 +1,36 @@
 from flask import Flask
 from flask import request
+from flask import make_response
+from hashlib import sha256
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+@app.route("/")
 def hash_api():
     return "Hello"
 
-@app.route('/hash', methods=['POST'])
+@app.route("/values", methods=["GET"])
+def hash_ap():
+    # file = open(r"db.txt", "w")
+    # file.write(hashedValue)
+    # file.close()
+    return val
+
+@app.route("/hash", methods=["POST"])
 def hash():
-    return request.args.get('username', '')
+    valueToHash = request.form.get("password")
+    s = json.dumps(valueToHash, sort_keys = True)
+    hashedValue = sha256(s.encode()).hexdigest()
+    response = make_response({"password": hashedValue})
+    response.headers["Content-Type"] = "application/json"
+    # file = open(r"db.txt", "w")
+    # file.write(hashedValue)
+    # file.close()
+    return response
 
 
 if __name__ == "__main__":
-    app.run(ssl_context='adhoc')
+    app.run(ssl_context="adhoc")
